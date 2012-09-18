40aec9ecabdf8cbd3b9b46a75564587fc92325f4
Chris Mowforth <chris@mowforth.com>
2012-09-18 23:47:25 +0100

    refactor JAR downloader, add guava dependency

--- lib/foxbat.rb
+++ lib/foxbat.rb
@@ -1,37 +1,35 @@
 require 'java'
 
-NETTY_VERSION = '3.5.0.Final'
-JAR_SIZE = 1110903.0 # TODO: remove hard-coded jar size
-
-netty_jar = File.dirname(__FILE__) + "/netty-#{NETTY_VERSION}.jar"
-
-if !File.exist?(netty_jar)
-  puts "Couldn't find netty's JAR to load. Let's download it now."
+def require_or_get(lib, version, ns)
+  jar = "#{lib}-#{version}.jar"
+  jarpath = File.dirname(__FILE__) + '/' + jar
+  if !File.exist?(jarpath)
+    puts "Couldn't find #{lib}, let's download it now."
   
-  require 'net/http'
-  f = open(netty_jar, 'w')
+    require 'net/http'
+    f = open(jarpath, 'w')
 
-  Net::HTTP.start('search.maven.org') do |http|
+    Net::HTTP.start('search.maven.org') do |http|
 
-    begin
-    http.request_get("/remotecontent?filepath=io/netty/netty/#{NETTY_VERSION}/netty-#{NETTY_VERSION}.jar") do |resp|
-    acc = 0
-    resp.read_body do |segment|
-          acc += segment.length
-          percent = ((acc / JAR_SIZE) * 100).to_i
-          print "Downloading netty #{NETTY_VERSION} from maven repository... [#{percent}%]\r"
-          f.write(segment)
+      begin
+        http.request_get("/remotecontent?filepath=#{ns}/#{lib}/#{version}/#{jar}") do |resp|
+          puts "Downloading #{jar} from maven repository..."
+          resp.read_body do |segment|
+            f.write(segment)
+          end
+          puts "Done."
         end
-        puts "\nDone. You shouldn't see me again :)"
+      ensure
+        f.close()
       end
-  ensure
-    f.close()
     end
   end
+
+  require jar
 end
 
-require 'netty-3.5.0.Final.jar'
-require 'guava-13.0.1.jar'
+require_or_get 'netty', '3.5.0.Final', 'io/netty'
+require_or_get 'guava', '13.0.1', 'com/google/guava'
 
 require 'em/connection'
 require 'em/deferrable'
